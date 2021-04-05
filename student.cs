cc1258a32fd5e4b8599e21178fd151794ec4be3e
Trần Văn Sơn <71358332+snleo882k@users.noreply.github.com>
2021-04-05 16:46:41 +0700

    Update student.cs

--- student.cs
+++ student.cs
@@ -1,4 +1,4 @@
-class Student
+ class Student
     {
 
         private string Name; // 1.truong
@@ -18,12 +18,12 @@ class Student
         //3. Phuong thuc (Method)
         public  Student()
         {
-            Console.WriteLine("Tên tôi là:");
+           
         }
         // 4. Phuong thuc(Method) Talk
         public void Talk(string stName)
         {
-            Console.WriteLine("tôi là :");
+            
         }
 
         private void btnTalk_Click(object sender, RoutedEventArgs e)
@@ -35,43 +35,3 @@ class Student
             st1.Talk();
         }
         
-        
-    }class Student
-    {
-
-        private string Name; // 1.truong
-
-        // 2. Thuoc tinh
-        public string StudentName
-        {
-            get
-            {
-                return Name;
-            }
-            set
-            {
-                Name = value;
-            }
-        }
-        //3. Phuong thuc (Method)
-        public  Student()
-        {
-            Console.WriteLine("Tên tôi là:");
-        }
-        // 4. Phuong thuc(Method) Talk
-        public void Talk(string stName)
-        {
-            Console.WriteLine("tôi là :");
-        }
-
-        private void btnTalk_Click(object sender, RoutedEventArgs e)
-        {
-            Student st1 = new Student("minh");
-            st1.Talk();
-            Student st2 = new Student();
-            st1.StudentName = "dung"
-            st1.Talk();
-        }
-        
-        
-    }
