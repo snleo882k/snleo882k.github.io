b87d8badcbf3d5dbb7cbc356e82bdaa69e8d4f72
Trần Văn Sơn <71358332+snleo882k@users.noreply.github.com>
2021-04-05 16:38:34 +0700

    Update student.cs

--- student.cs
+++ student.cs
@@ -1,12 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AppOOP
-{
-    class Student
+class Student
     {
 
         private string Name; // 1.truong
@@ -34,16 +26,52 @@ namespace AppOOP
             Console.WriteLine("tôi là :");
         }
 
-
-        
-        static void Main(string[] args)
+        private void btnTalk_Click(object sender, RoutedEventArgs e)
         {
-            Student h1 = new Student();
-           
-            
-
-
+            Student st1 = new Student("minh");
+            st1.Talk();
+            Student st2 = new Student();
+            st1.StudentName = "dung"
+            st1.Talk();
         }
-    }
-}
+        
+        
+    }class Student
+    {
 
+        private string Name; // 1.truong
+
+        // 2. Thuoc tinh
+        public string StudentName
+        {
+            get
+            {
+                return Name;
+            }
+            set
+            {
+                Name = value;
+            }
+        }
+        //3. Phuong thuc (Method)
+        public  Student()
+        {
+            Console.WriteLine("Tên tôi là:");
+        }
+        // 4. Phuong thuc(Method) Talk
+        public void Talk(string stName)
+        {
+            Console.WriteLine("tôi là :");
+        }
+
+        private void btnTalk_Click(object sender, RoutedEventArgs e)
+        {
+            Student st1 = new Student("minh");
+            st1.Talk();
+            Student st2 = new Student();
+            st1.StudentName = "dung"
+            st1.Talk();
+        }
+        
+        
+    }
